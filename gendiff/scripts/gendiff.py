f67d62c1c6d654ab0334f433a7767d341926045f
Sergei Mikurov <454004@mail.ru>
2023-04-06 15:54:40 +0500

    fix script gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,13 +6,13 @@ from gendiff.gendiff import generate_gendiff
 def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference'
-    )
+        )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument(
         '-f', '--format',
         help='set format of output'
-    )
+        )
     args = parser.parse_args()
 
     print(generate_gendiff(args.first_file, args.second_file))
