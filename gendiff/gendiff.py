ef570b3ab0233469e8cf24978640ab6d5c69a48e
Sergei Mikurov <454004@mail.ru>
2023-04-06 15:53:01 +0500

    fix module gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -16,17 +16,16 @@ def generate_gendiff(file1, file2):
     dct_from_file1 = json.load(open(file1))
     dct_from_file2 = json.load(open(file2))
 
-    lst_of_keys = list(set(
-        list(dct_from_file1.keys()) + list(dct_from_file2.keys()))
+    lst_of_keys = list(
+        set(list(dct_from_file1.keys())) | set(list(dct_from_file2.keys()))
         )
     lst_of_keys.sort()
 
     result_strings = '{\n'
     for key in lst_of_keys:
-        string_of_diff = create_a_string_of_diff(
+        result_strings += create_a_string_of_diff(
             dct_from_file1, dct_from_file2, key
             )
-        result_strings += string_of_diff
     result_strings += '}'
 
     return result_strings
