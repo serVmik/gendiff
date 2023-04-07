c3949e878d9b4f08bc0c4ca6aed950ea0c92160f
Sergei Mikurov <454004@mail.ru>
2023-04-08 00:37:52 +0500

    fix tests/test_generate_gendiff.py

--- tests/test_generate_gendiff.py
+++ tests/test_generate_gendiff.py
@@ -1,10 +1,10 @@
-from gendiff.module_gendiff import generate_gendiff
+from gendiff.module_gendiff import generate_gendiff, create_a_string_of_diff
 
-file1_json_for_test = 'fixtures/file1_for_test.json'
-file2_json_for_test = 'fixtures/file2_for_test.json'
+file1_json_for_test = 'tests/fixtures/file1_for_test.json'
+file2_json_for_test = 'tests/fixtures/file2_for_test.json'
 
 
 def test_gendiff():
-    expected = open('fixtures/expected_from_test_file1_file2.txt', 'r').read()
+    expected = open('tests/fixtures/expected_from_test_file1_file2.txt', 'r').read()
     function_for_test = generate_gendiff(file1_json_for_test, file2_json_for_test)
     assert function_for_test == expected
