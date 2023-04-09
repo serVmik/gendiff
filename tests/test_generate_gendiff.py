eb1ee001351ed4780c1f8d94caf9a7ec10030eb5
Sergei Mikurov <454004@mail.ru>
2023-04-09 21:14:14 +0500

    add yaml to test

--- tests/test_generate_gendiff.py
+++ tests/test_generate_gendiff.py
@@ -1,19 +1,15 @@
 import pytest
-import gendiff.gendiff_module
-import gendiff.gendiff_parser
+import gendiff.gendiff_module as gendiff_
 
-file1_json_for_test = 'tests/fixtures/file1_for_test.json'
-file2_json_for_test = 'tests/fixtures/file2_for_test.json'
+file1_json = 'tests/fixtures/file1_for_test.json'
+file2_json = 'tests/fixtures/file2_for_test.json'
+file1_yaml = 'tests/fixtures/file1_for_test.yaml'
+file2_yml = 'tests/fixtures/file2_for_test.yml'
 
 
 @pytest.mark.parametrize("file1, file2", [
-    (file1_json_for_test, file2_json_for_test)
+    (file1_json, file2_json), (file1_yaml, file2_yml)
 ])
-
-
 def test_gendiff(file1, file2):
     with open('tests/fixtures/expected_from_test_file1_file2.txt', 'r') as expected:
-        function_for_test = gendiff.gendiff_module.generate_gendiff(
-            file1_json_for_test, file2_json_for_test
-            )
-        assert function_for_test == expected.read()
+        assert gendiff_.generate_gendiff(file1, file2) == expected.read()
