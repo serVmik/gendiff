aa9e70efcbdede2c8b199d7c60aadb828fc06724
Sergei Mikurov <454004@mail.ru>
2023-04-15 21:31:12 +0500

    fix gendiff_parser.py

--- gendiff/gendiff_parser.py
+++ gendiff/gendiff_parser.py
@@ -1,9 +1,17 @@
-def create_a_string_of_diff(dct1, dct2, key):
-    if dct1.get(key) == dct2.get(key):
-        return f'    {key}: {dct1[key]}\n'
-    elif key in dct1 and key in dct2:
-        return f'  - {key}: {dct1[key]}\n  + {key}: {dct2[key]}\n'
-    elif key in dct1:
-        return f'  - {key}: {dct1[key]}\n'
-    else:
-        return f'  + {key}: {dct2[key]}\n'
+def create_a_string_of_diff(dct1, dct2, lst_of_actions_for_dct_key):
+    result_strings = '{\n'
+    for action_for_dct_key in lst_of_actions_for_dct_key:
+        key = action_for_dct_key[0]
+        action = action_for_dct_key[1]
+        if action == 'equal':
+            result_strings += f'    {key}: {dct1[key]}\n'
+        elif action == 'changed':
+            result_strings += f'  - {key}: {dct1[key]}\n'\
+                            + f'  + {key}: {dct2[key]}\n'
+        elif action == 'removed':
+            result_strings += f'  - {key}: {dct1[key]}\n'
+        else:       # action == 'added'
+            result_strings += f'  + {key}: {dct2[key]}\n'
+    result_strings += '}'
+
+    return result_strings
