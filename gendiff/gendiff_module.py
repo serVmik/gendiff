11fe70bd396ab45bb27ec36da4144bf7f8254015
Sergei Mikurov <454004@mail.ru>
2023-04-15 19:02:18 +0500

    fix gendiff_module.py

--- gendiff/gendiff_module.py
+++ gendiff/gendiff_module.py
@@ -5,7 +5,6 @@ from gendiff.gendiff_parser import create_a_string_of_diff
 
 def get_dct_from_file(file_):
     type_of_file = file_[-file_[::-1].find('.'):]
-    dct_from_file = dict()
 
     if type_of_file == 'json':
         dct_from_file = json.load(open(file_))
@@ -20,15 +19,21 @@ def get_dct_from_file(file_):
 def generate_gendiff(file1, file2):
     dct_from_file1 = get_dct_from_file(file1)
     dct_from_file2 = get_dct_from_file(file2)
-    lst_of_keys = sorted(list(
-        set(dct_from_file1.keys()).union(set(dct_from_file2.keys()))
+    lst_of_actions_for_dct_key = sorted(list(
+        set(dct_from_file1.keys()).union(dct_from_file2.keys())
     ))
 
-    result_strings = '{\n'
-    for key in lst_of_keys:
-        result_strings += create_a_string_of_diff(
-            dct_from_file1, dct_from_file2, key
-        )
-    result_strings += '}'
+    for index, key in enumerate(lst_of_actions_for_dct_key):
+        if dct_from_file1.get(key) == dct_from_file2.get(key):
+            lst_of_actions_for_dct_key[index] = (key, 'equal')
+        elif key in dct_from_file1 and key in dct_from_file2:
+            lst_of_actions_for_dct_key[index] = (key, 'changed')
+        elif key in dct_from_file1:
+            lst_of_actions_for_dct_key[index] = (key, 'removed')
+        else:
+            lst_of_actions_for_dct_key[index] = (key, 'added')
 
-    return result_strings
+    result_string = create_a_string_of_diff(
+        dct_from_file1, dct_from_file2, lst_of_actions_for_dct_key)
+
+    return result_string
