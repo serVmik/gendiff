0acbd01c6b7e9871b6d2c666726ba0de69b90def
Sergei Mikurov <454004@mail.ru>
2023-05-14 11:11:43 +0500

    add format plain
    add gendiff/formatters/output_plain.py
    add tests/fixtures/expected_output_plain.txt
    add tests/test_output_plain.py

---
+++ tests/test_output_plain.py
@@ -0,0 +1,11 @@
+# from gendiff.formatters.output_plain import create_output_plain
+# from tests.fixtures import expected_parse_stylish
+#
+#
+# def test_create_output_plain():
+#     with open('tests/fixtures/expected_output_plain.txt', 'r') as expected:
+#         assert create_output_plain(
+#             expected_parse_stylish.dct1,
+#             expected_parse_stylish.dct2,
+#             expected_parse_stylish.lst_of_diff
+#         ) == expected.read()
