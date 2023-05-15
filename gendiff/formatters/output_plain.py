b6e18355115874fd4e9f17c6dd2379cbbf623d20
Sergei Mikurov <454004@mail.ru>
2023-05-15 13:51:21 +0500

    fix output_plain.py

--- gendiff/formatters/output_plain.py
+++ gendiff/formatters/output_plain.py
@@ -1,4 +1,22 @@
-def create_output_plain(dct1, dct2, lst_of_diff):
-    lst_of_lines = []
+# result_line = f"Property {path} was {action}{old_value}{new_value}"
+import os
 
-    return lst_of_lines
+
+def create_lines_of_diff(lst_of_diff):
+    def walk(node, path):
+        path = os.path.join(path, node.get('property')).replace('/', '.')
+        if 'nested' not in node:
+            print(f'{path}')
+            return
+
+        lst_of_nested_property = node.get('nested')
+        list(map(lambda item: walk(item, path), lst_of_nested_property))
+
+    for element in lst_of_diff:
+        walk(element, '')
+
+    return
+
+
+def create_output_plain(dct1, dct2, lst_of_diff):
+    create_lines_of_diff(lst_of_diff)
