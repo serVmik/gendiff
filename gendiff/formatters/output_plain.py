221b0d1c0823e265a1555590b49c6bdf2a056cff
Sergei Mikurov <454004@mail.ru>
2023-05-15 23:17:49 +0500

    fix output_plain.py

--- gendiff/formatters/output_plain.py
+++ gendiff/formatters/output_plain.py
@@ -1,22 +1,37 @@
 # result_line = f"Property {path} was {action}{old_value}{new_value}"
-import os
 
 
-def create_lines_of_diff(lst_of_diff):
-    def walk(node, path):
-        path = os.path.join(path, node.get('property')).replace('/', '.')
-        if 'nested' not in node:
-            print(f'{path}')
-            return
+def get_value_from_dct_using_path(dct_input, path_input):
+    received_value = []
+    start_index_of_key_along_path = 0
 
-        lst_of_nested_property = node.get('nested')
-        list(map(lambda item: walk(item, path), lst_of_nested_property))
+    def walk(dct, path, index):
+        key = path[index]
+        value = dct.get(key)
 
-    for element in lst_of_diff:
-        walk(element, '')
+        if len(path) > 1 + index:
+            walk(value, path, index + 1)
 
-    return
+        received_value.append(value)
+
+    walk(dct_input, path_input, start_index_of_key_along_path)
+    return received_value[0]
 
 
 def create_output_plain(dct1, dct2, lst_of_diff):
-    create_lines_of_diff(lst_of_diff)
+    def walk(value1, value2, node, path):
+        path += (node.get('property'),)
+        action = node.get('marker')
+
+        if 'nested' not in node and action == 'added':
+            value = get_value_from_dct_using_path(dct2, path)
+            print(f"Property '{'.'.join(path)}' was {value}")
+
+        lst_of_nested_property = node.get('nested', ())
+        list(map(
+            lambda item: walk(dct1, dct2, item, path),
+            lst_of_nested_property
+        ))
+
+    for element in lst_of_diff:
+        walk(dct1, dct2, element, ())
