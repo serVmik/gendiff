d7fc54e8e8130f860800106c1a62290332abf6ab
Sergei Mikurov <454004@mail.ru>
2023-05-17 00:02:35 +0500

    fix output_plain.py

--- gendiff/formatters/output_plain.py
+++ gendiff/formatters/output_plain.py
@@ -3,45 +3,45 @@ from gendiff.gendiff_tools import convert_to_string
 from gendiff.gendiff_tools import get_value_using_path
 
 
-def create_output_plain(dct1_input, dct2_input, lst_of_diff):
+def create_output_plain(dct1, dct2, lst_of_diff):
     lst_of_lines = []
 
     def walk(node, path):
         path += (node.get('property'),)
-        action = node.get('marker')
+        marker = node.get('marker')
 
-        if 'nested' not in node and action == 'added':
-            value2 = convert_to_string(get_value_using_path(dct2_input, path))
+        if 'nested' not in node and marker == 'added':
+            value2 = convert_to_string(get_value_using_path(dct2, path))
             quote = add_quote_to_string(value2)
 
             line = f"Property '{'.'.join(path)}' was added with value: " \
                    f"{quote}{value2}{quote}"
             lst_of_lines.append(line)
 
-        elif action == 'added':
+        elif marker == 'added':
             line = f"Property '{'.'.join(path)}' was added with value: " \
                    f"[complex value]"
             lst_of_lines.append(line)
 
-        elif 'nested' not in node and action == 'changed':
-            value1 = convert_to_string(get_value_using_path(dct1_input, path))
+        elif 'nested' not in node and marker == 'changed':
+            value1 = convert_to_string(get_value_using_path(dct1, path))
             quote1 = add_quote_to_string(value1)
-            value2 = convert_to_string(get_value_using_path(dct2_input, path))
+            value2 = convert_to_string(get_value_using_path(dct2, path))
             quote2 = add_quote_to_string(value2)
 
             line = f"Property '{'.'.join(path)}' was updated. " \
                    f"From {quote1}{value1}{quote1} to {quote2}{value2}{quote2}"
             lst_of_lines.append(line)
 
-        elif action == 'changed':
-            value2 = convert_to_string(get_value_using_path(dct2_input, path))
+        elif marker == 'changed':
+            value2 = convert_to_string(get_value_using_path(dct2, path))
             quote = add_quote_to_string(value2)
 
             line = f"Property '{'.'.join(path)}' was updated. " \
                    f"From [complex value] to {quote}{value2}{quote}"
             lst_of_lines.append(line)
 
-        elif action == 'removed':
+        elif marker == 'removed':
             line = f"Property '{'.'.join(path)}' was removed"
             lst_of_lines.append(line)
 
