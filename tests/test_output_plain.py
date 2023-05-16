eb905ab3e0f4f76366c94283d815b6c36916b9e6
Sergei Mikurov <454004@mail.ru>
2023-05-16 13:29:58 +0500

    fix test_output_plain
    fix tests/fixtures/expected_output_plain.txt
    fix tests/test_output_plain.py

--- tests/test_output_plain.py
+++ tests/test_output_plain.py
@@ -1,11 +1,11 @@
-# from gendiff.formatters.output_plain import create_output_plain
-# from tests.fixtures import expected_parse_stylish
-#
-#
-# def test_create_output_plain():
-#     with open('tests/fixtures/expected_output_plain.txt', 'r') as expected:
-#         assert create_output_plain(
-#             expected_parse_stylish.dct1,
-#             expected_parse_stylish.dct2,
-#             expected_parse_stylish.lst_of_diff
-#         ) == expected.read()
+from gendiff.formatters.output_plain import create_output_plain
+from tests.fixtures import expected_parser_plain
+
+
+def test_create_output_plain():
+    with open('tests/fixtures/expected_output_plain.txt', 'r') as expected:
+        assert create_output_plain(
+            expected_parser_plain.dct1,
+            expected_parser_plain.dct2,
+            expected_parser_plain.lst_dcts_of_diff
+        ) == expected.read()
