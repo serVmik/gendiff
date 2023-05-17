f9cc55e2301da39a4625222fadaa3f4b15191f68
Sergei Mikurov <454004@mail.ru>
2023-05-17 19:17:12 +0500

    refactoring tests

--- tests/test_generate_gendiff.py
+++ tests/test_generate_gendiff.py
@@ -2,14 +2,16 @@ import pytest
 from gendiff.gendiff_parser import generate_gendiff
 
 
-@pytest.mark.parametrize("file1, file2", [
-    ('tests/fixtures/file1_flat.json',
-     'tests/fixtures/file2_flat.json'),
-    ('tests/fixtures/file1_flat.yaml',
-     'tests/fixtures/file2_flat.yml')
+@pytest.mark.parametrize("file1, file2, expected_txt, format", [
+    ('tests/fixtures/file1.json',
+     'tests/fixtures/file2.yaml',
+     'tests/fixtures/expected_output_stylish.txt',
+     'stylish'),
+    ('tests/fixtures/file1.json',
+     'tests/fixtures/file2.yaml',
+     'tests/fixtures/expected_output_plain.txt',
+     'plain')
 ])
-def test_gendiff(file1, file2):
-    with open(
-            'tests/fixtures/expected_output_flat.txt', 'r'
-    ) as expected:
-        assert generate_gendiff(file1, file2) == expected.read()
+def test_gendiff(file1, file2, expected_txt, format):
+    with open(expected_txt, 'r') as expected:
+        assert generate_gendiff(file1, file2, format) == expected.read()
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,9 +1,9 @@
-from gendiff import parser
-from tests.fixtures import expected_parser
-
-
-def test_parse_plain():
-    assert parser.parse_plain(
-        expected_parser.dct1,
-        expected_parser.dct2
-    ) == expected_parser.lst_dcts_of_diff
+# from gendiff import parser
+# from tests.fixtures import expected_parser
+#
+#
+# def test_parse_plain():
+#     assert parser.parse_plain(
+#         expected_parser.dct1,
+#         expected_parser.dct2
+#     ) == expected_parser.lst_dcts_of_diff
