fa2db38bc2b006731353df2ac9347e319ac70ed8
Sergei Mikurov <454004@mail.ru>
2023-05-18 17:17:09 +0500

    fix output_plain.py

--- gendiff/formatters/output_plain.py
+++ gendiff/formatters/output_plain.py
@@ -6,27 +6,31 @@ from gendiff.gendiff_tools import get_value_using_path
 def create_line_with_changed(dct1, dct2, path, lst_dcts_of_diff):
     value2 = convert_to_string(get_value_using_path(dct2, path))
     quote2 = create_quote_for_string(value2)
+    value1 = convert_to_string(get_value_using_path(dct1, path))
+    quote1 = create_quote_for_string(value1)
 
-    if 'nested' not in lst_dcts_of_diff:
-        value1 = convert_to_string(get_value_using_path(dct1, path))
-        quote1 = create_quote_for_string(value1)
+    if 'nested' in lst_dcts_of_diff:
+        if not isinstance(value1, dict):
+            return f"Property '{'.'.join(path)}' was updated. " \
+                   f"From [complex value] to {quote2}{value2}{quote2}"
+        else:
+            return f"Property '{'.'.join(path)}' was updated. " \
+                   f"From {quote1}{value1}{quote1} to [complex value]"
+    else:
         return f"Property '{'.'.join(path)}' was updated. "\
                f"From {quote1}{value1}{quote1} to {quote2}{value2}{quote2}"
-    else:
-        return f"Property '{'.'.join(path)}' was updated. " \
-               f"From [complex value] to {quote2}{value2}{quote2}"
 
 
 def create_line_with_added(dct2, path, lst_dcts_of_diff):
     value2 = convert_to_string(get_value_using_path(dct2, path))
     quote2 = create_quote_for_string(value2)
 
-    if 'nested' not in lst_dcts_of_diff:
-        return f"Property '{'.'.join(path)}' was added with value: " \
-               f"{quote2}{value2}{quote2}"
-    else:
+    if 'nested' in lst_dcts_of_diff:
         return f"Property '{'.'.join(path)}' was added with value: " \
                f"[complex value]"
+    else:
+        return f"Property '{'.'.join(path)}' was added with value: " \
+               f"{quote2}{value2}{quote2}"
 
 
 def create_output_plain(dct1, dct2, lst_dcts_of_diff_input):
